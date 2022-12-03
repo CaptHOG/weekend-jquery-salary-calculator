05f697b0337dd1a2f56a7c753ee7a3f331136cab
Jeremy Hanson <rockbasser88@hotmail.com>
2022-12-03 13:54:34 -0600

    submit button adds new info to table body

--- script.js
+++ script.js
@@ -7,13 +7,6 @@ let employees = [
         idNumber: 1234,
         title: 'CEO',
         annualSalary: 100000
-    },
-    {
-        firstName: 'Jane',
-        lastName: 'Doe',
-        idNumber: 2345,
-        title: 'CFO',
-        annualSalary: 200000
     }
 ];
 
@@ -23,19 +16,63 @@ function onReady() {
     $('#submitEmployeeButton').on('click', addEmployee)
 }
 
+// re-renders the table body
 function renderEmployeeList() {
     $('.employeeList').empty();
-    $('.employeeList').append(`
-    <td>John</td>
-    <td>Doe</td>
-    <td>1234</td>
-    <td>CEO</td>
-    <td>$100,000</td>
-    <td><button>Delete</button></td>
-    `);
+    for (i=0; i<employees.length; i++) {
+        if (i === 0) {
+            $('.employeeList').append(`
+            <tr>
+                <td>${employees[i].firstName}</td>
+                <td>${employees[i].lastName}</td>
+                <td>${employees[i].idNumber}</td>
+                <td>${employees[i].title}</td>
+                <td>${employees[i].annualSalary}</td>
+                <td><button>Delete</button></td>
+            </tr>
+            `);
+        } else {
+            $('.employeeList').append(`
+            <tr>
+                <td>${employees[i].firstName}</td>
+                <td>${employees[i].lastName}</td>
+                <td>${employees[i].idNumber}</td>
+                <td>${employees[i].title}</td>
+                <td>${employees[i].annualSalary}</td>
+                <td><button>Delete</button></td>
+            </tr>
+            `);
+        }
+    }
     
 }
 
+// adds new employee info to employees array
 function addEmployee() {
     console.log('employee added');
+    let newFirstName = $('#firstNameInput').val();
+    let newLastName = $('#lastNameInput').val();
+    let newId = $('#idInput').val();
+    let newTitle = $('#titleInput').val();
+    let newAnnualSalary = $('#annualSalaryInput').val();
+
+    let newEmployee = {
+        firstName: newFirstName,
+        lastName: newLastName,
+        idNumber: Number(newId),
+        title: newTitle,
+        annualSalary: Number(newAnnualSalary)
+    };
+
+    employees.push(newEmployee);
+    renderEmployeeList();
+
+    $('#firstNameInput').val('');
+    $('#lastNameInput').val('');
+    $('#idInput').val('');
+    $('#titleInput').val('');
+    $('#annualSalaryInput').val('');
+
+    console.log(newEmployee);
+    // console.log(employees);
 }
