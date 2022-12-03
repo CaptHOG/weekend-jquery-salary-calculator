f7f9ea37e971ec1b331b3187e8c9dc4cc4fdc455
Jeremy Hanson <rockbasser88@hotmail.com>
2022-12-03 12:09:39 -0600

    added table

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
 $(document).ready(onReady);
 
+let employees = [
+    {
+        firstName: 'John',
+        lastName: 'Doe',
+        idNumber: 1234,
+        title: 'CEO',
+        annualSalary: 100000
+    },
+    {
+        firstName: 'Jane',
+        lastName: 'Doe',
+        idNumber: 2345,
+        title: 'CFO',
+        annualSalary: 200000
+    }
+];
+
 function onReady() {
-    
+
+}
+
+function renderEmployeeList() {
+    $('.employeeList').empty();
 }
